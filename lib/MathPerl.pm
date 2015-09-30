512e1a65fd96c415c525439aa2d26f12961e68af
Will Braswell <william.braswell@autoparallel.com>
2015-09-30 01:46:04 -0500

    Initial Commit; Split Geometric Algebra From RPerl, Add All Euclidean Product Tests

---
+++ lib/MathPerl.pm
@@ -0,0 +1,15 @@
+# [[[ HEADER ]]]
+package MathPerl;
+use strict;
+use warnings;
+use RPerl::AfterSubclass;
+our $VERSION = 0.000_001;
+
+# [[[ OO INHERITANCE ]]]
+use parent qw(RPerl::CompileUnit::Module::Class);  # no non-system inheritance, only inherit from base class
+use RPerl::CompileUnit::Module::Class;
+
+# [[[ OO PROPERTIES ]]]
+our hashref $properties = {};
+
+1;    # end of class
---
+++ lib/MathPerl/GeometricAlgebra.pm
@@ -0,0 +1,15 @@
+# [[[ HEADER ]]]
+package MathPerl::GeometricAlgebra;
+use strict;
+use warnings;
+use RPerl::AfterSubclass;
+our $VERSION = 0.000_001;
+
+# [[[ OO INHERITANCE ]]]
+use parent qw(MathPerl);
+use MathPerl;
+
+# [[[ OO PROPERTIES ]]]
+our hashref $properties = {};
+
+1;    # end of class

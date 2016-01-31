429396ba61855a8aba913c625a3ea2a3c0b00f41
Will Braswell <william.braswell@autoparallel.com>
2016-01-31 10:34:47 -0600

    Fix Missing Includes For Products.pm

--- lib/MathPerl/GeometricAlgebra.pm
+++ lib/MathPerl/GeometricAlgebra.pm
@@ -3,11 +3,14 @@ package MathPerl::GeometricAlgebra;
 use strict;
 use warnings;
 use RPerl::AfterSubclass;
-our $VERSION = 0.000_001;
+our $VERSION = 0.000_003;
 
 # [[[ OO INHERITANCE ]]]
 use parent qw(MathPerl);
 use MathPerl;
+
+# [[[ INCLUDES ]]]
+use MathPerl::GeometricAlgebra::Products;
 
 # [[[ OO PROPERTIES ]]]
 our hashref $properties = {};

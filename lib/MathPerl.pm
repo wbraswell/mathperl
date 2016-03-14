51e9f80110e46bcc60ac1b5033b40a7404a15725
Will Braswell <william.braswell@autoparallel.com>
2016-03-14 07:58:14 -0500

    CPAN Release, v0.100000; Codename Cherry, Pi Day Edition

--- lib/MathPerl.pm
+++ lib/MathPerl.pm
@@ -3,7 +3,7 @@ package MathPerl;
 use strict;
 use warnings;
 use RPerl::AfterSubclass;
-our $VERSION = 0.000_001;
+our $VERSION = 0.100_000;
 
 # [[[ OO INHERITANCE ]]]
 use parent qw(RPerl::CompileUnit::Module::Class);  # no non-system inheritance, only inherit from base class

c2c126d34b08de70b6b7e90db488ba92b293c4cb
Will Braswell <william.braswell@autoparallel.com>
2016-06-23 05:33:06 -0500

    Vector Algebra, Cross Product, Part 3

--- lib/MathPerl/VectorAlgebra/CrossProduct.pm
+++ lib/MathPerl/VectorAlgebra/CrossProduct.pm
@@ -3,7 +3,7 @@ use RPerl;
 package MathPerl::VectorAlgebra::CrossProduct;
 use strict;
 use warnings;
-our $VERSION = 0.002_000;
+our $VERSION = 0.003_000;
 
 # [[[ OO INHERITANCE ]]]
 use parent qw(MathPerl::Operation);
@@ -25,29 +25,51 @@ our hashref $properties = {};
 our Vector3D $cross_product_vector3d = sub {
     ( my Vector3D $u, my Vector3D $v ) = @_;
     my Vector3D $retval = Vector3D->new();
-    $retval->set_x(($u->get_y() * $v->get_z()) - ($u->get_z() * $v->get_y()));
-    $retval->set_y(($u->get_z() * $v->get_x()) - ($u->get_x() * $v->get_z()));
-    $retval->set_z(($u->get_x() * $v->get_y()) - ($u->get_y() * $v->get_x()));
+
+    # NEED ADD ERROR CHECKING, if elements are all defined
+
+    # OO properties direct access mechanism, does NOT work if dependency files are compiled but this file is not
+    $retval->{x} = ($u->{y} * $v->{z}) - ($u->{z} * $v->{y});
+    $retval->{y} = ($u->{z} * $v->{x}) - ($u->{x} * $v->{z});
+    $retval->{z} = ($u->{x} * $v->{y}) - ($u->{y} * $v->{x});
+
+    # OO properties accessor/mutator mechanism, DOES work if dependency files are compiled but this file is not
+#    $retval->set_x(($u->get_y() * $v->get_z()) - ($u->get_z() * $v->get_y()));
+#    $retval->set_y(($u->get_z() * $v->get_x()) - ($u->get_x() * $v->get_z()));
+#    $retval->set_z(($u->get_x() * $v->get_y()) - ($u->get_y() * $v->get_x()));
     return $retval;
 };
 
 our Vector3D $cross_product_vector = sub {
     ( my Vector $u, my Vector $v ) = @_;
-    my integer $dimensions = scalar $u->get_head();
-    # NEED ADD: check if dims of u match v, die otherwise
-    
-    my number_arrayref $u_head = $u->get_head();
-    my number_arrayref $v_head = $v->get_head();
+
+    # OO properties direct access mechanism
+    my number_arrayref $u_head = $u->{head};
+    my number_arrayref $v_head = $v->{head};
+    my integer $u_dimensions = scalar $u_head;
+    my integer $v_dimensions = scalar $v_head;
+ 
+    # OO properties accessor/mutator mechanism
+#    my number_arrayref $u_head = $u->get_head();
+#    my number_arrayref $v_head = $v->get_head();
+#    my integer $u_dimensions = scalar $u_head;
+#    my integer $v_dimensions = scalar $v_head;
+
+    if ($u_dimensions != $v_dimensions) {
+        croak 'ERROR EMPVACPxx: Dimension mis-match, input vector $u contains ' . $u_dimensions . 
+            ' elements and input vector $v contains ' . $v_dimensions . ' elements, croaking';
+    }
 
     my Vector $retval = Vector->new();
-    my number_arrayref $retval_head = $retval->get_head();
+    my number_arrayref $retval_head = $retval->{head};  # OO properties direct access mechanism
+#    my number_arrayref $retval_head = $retval->get_head();  # OO properties accessor/mutator mechanism
 
-    if ($dimensions == 3) {
+    if ($u_dimensions == 3) {
         $retval_head->[0] = ($u_head->[1] * $v_head->[2]) - ($u_head->[2] * $v_head->[1]);
         $retval_head->[1] = ($u_head->[2] * $v_head->[0]) - ($u_head->[0] * $v_head->[2]);
         $retval_head->[2] = ($u_head->[0] * $v_head->[1]) - ($u_head->[1] * $v_head->[0]);
     }
-    elsif ($dimensions == 7) {
+    elsif ($u_dimensions == 7) {
         $retval_head->[0] = (($u_head->[1] * $v_head->[3]) - ($u_head->[3] * $v_head->[1])) + 
                             (($u_head->[2] * $v_head->[6]) - ($u_head->[6] * $v_head->[2])) +
                             (($u_head->[4] * $v_head->[5]) - ($u_head->[5] * $v_head->[4]));

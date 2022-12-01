d1fa35c879cc2062c53aceec0c6db8d92b06dbc9
Will Braswell <william.braswell@autoparallel.com>
2022-11-30 23:41:06 -0600

    Fractal, Sierpinski Triangle, Corrected Recursion

--- script/sierpinski_triangle.pl
+++ script/sierpinski_triangle.pl
@@ -3,30 +3,19 @@
 # [[[ HEADER ]]]
 use strict;
 use warnings;
-our $VERSION = 0.001_000;
+our $VERSION = 0.002_000;
 
 # [[[ CRITICS ]]]
 ## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls)  # USER DEFAULT 1: allow numeric values & print operator
 ## no critic qw(RequireInterpolationOfMetachars)  # USER DEFAULT 2: allow single-quoted control characters & sigils
 ## no critic qw(ProhibitConstantPragma ProhibitMagicNumbers)  # USER DEFAULT 3: allow constants
 
-package void;
-1;
-
-package integer;
-1;
-
-package number::arrayref;
-1;
-
-package number::arrayref::arrayref;
-1;
-
-package number::arrayref::arrayref::arrayref;
-1;
-
-package number::arrayref::arrayref::arrayref::arrayref;
-1;
+package void; 1;
+package integer; 1; 
+package number::arrayref; 1;
+package number::arrayref::arrayref; 1;
+package number::arrayref::arrayref::arrayref; 1;
+package number::arrayref::arrayref::arrayref::arrayref; 1;
 
 package main;
 use English;
@@ -44,70 +33,85 @@ my number::arrayref::arrayref $my_colors =
      [0.2, 0.2, 0.2],   # brown
      [1.0, 0.0, 0.0],   # red
      [1.0, 1.0, 1.0]];  # white
+my integer $my_recursions_remaining = scalar(@{$my_colors});
 
-# generate triangles grouped by recursion level
-my number::arrayref::arrayref::arrayref::arrayref $sierpinski_triangles = sierpinski($my_triangle, scalar(@{$my_colors}));
+# declare & initialize final array outside of the recursive subroutine for easy direct access by all recursive calls
+my number::arrayref::arrayref::arrayref::arrayref $my_triangle_groups = [];
+
+# initial triangle is in a triangle group by itself
+$my_triangle_groups->[$my_recursions_remaining] = [$my_triangle];
+
+# initial call to recursive subroutine
+sierpinski($my_triangle, $my_recursions_remaining, $my_triangle_groups);
+
+print 'have $my_triangle_groups = ', "\n", Dumper($my_triangle_groups);
+print 'have $my_triangle_groups->[$my_recursions_remaining] = ', "\n", Dumper($my_triangle_groups->[$my_recursions_remaining]);
 
 #foreach ... {
     # DRAW TRIANGLES
 #}
 
 
+# recursively generate triangles grouped by recursion level
 sub sierpinski {
-    { my number::arrayref::arrayref::arrayref $RETURN_TYPE };
+    { my void $RETURN_TYPE };
     (
         my number::arrayref::arrayref $triangle,
-        my integer $recursions_remaining
+        my integer $recursions_remaining,
+        my number::arrayref::arrayref::arrayref::arrayref $triangle_groups
     ) = @ARG;
 
     print 'in sierpinski(), received $triangle = ', Dumper($triangle), "\n";
     print 'in sierpinski(), received $recursions_remaining = ', $recursions_remaining, "\n";
 
-# START HERE: need correct initialization for $return_value; add debugging statements at recurse & return; get correct triangle data
-# START HERE: need correct initialization for $return_value; add debugging statements at recurse & return; get correct triangle data
-# START HERE: need correct initialization for $return_value; add debugging statements at recurse & return; get correct triangle data
 
-    my number::arrayref::arrayref $return_value = [ [ ] ];
+# START HERE: add debugging statements at recurse & return; get correct triangle data; draw triangles
+# START HERE: add debugging statements at recurse & return; get correct triangle data; draw triangles
+# START HERE: add debugging statements at recurse & return; get correct triangle data; draw triangles
+
 
     if ($recursions_remaining > 0) {
-        $recursions_remaining--;
 
         # shortcut variables, easier to read in midpoint calculations below
         my number::arrayref $point_a = $triangle->[0];
         my number::arrayref $point_b = $triangle->[1];
         my number::arrayref $point_c = $triangle->[2];
 
-        # midpoint between two coordinates [x1, y1] and [x2, y2] is [(x1+x2)/2, (y1+y2)/2]
-        my number::arrayref::arrayref $subtriangle_a =
-            [   $point_a,
-             [(($point_a->[0] + $point_b->[0]) / 2), 
-              (($point_a->[1] + $point_b->[1]) / 2)],
-             [(($point_a->[0] + $point_c->[0]) / 2), 
-              (($point_a->[1] + $point_c->[1]) / 2)]];
-        my number::arrayref::arrayref $subtriangle_b =
-            [[(($point_a->[0] + $point_b->[0]) / 2), 
-              (($point_a->[1] + $point_b->[1]) / 2)],
-                $point_b,
-             [(($point_b->[0] + $point_c->[0]) / 2), 
-              (($point_b->[1] + $point_c->[1]) / 2)]];
-        my number::arrayref::arrayref $subtriangle_c =
-            [[(($point_a->[0] + $point_c->[0]) / 2), 
-              (($point_a->[1] + $point_c->[1]) / 2)],
-             [(($point_b->[0] + $point_c->[0]) / 2), 
-              (($point_b->[1] + $point_c->[1]) / 2)],
-                $point_c                        ];
+        # calculate midpoints between two coordinates [x1, y1] and [x2, y2] is [(x1+x2)/2, (y1+y2)/2]
+        my number::arrayref $point_a_b =
+            [(($point_a->[0] + $point_b->[0]) / 2), 
+             (($point_a->[1] + $point_b->[1]) / 2)];
+        my number::arrayref $point_a_c =
+            [(($point_a->[0] + $point_c->[0]) / 2), 
+             (($point_a->[1] + $point_c->[1]) / 2)];
+        my number::arrayref $point_b_c =
+            [(($point_b->[0] + $point_c->[0]) / 2), 
+             (($point_b->[1] + $point_c->[1]) / 2)];
 
-        # recurse
-        push @{$return_value}, 
-        [
-            sierpinski( $subtriangle_a, $recursions_remaining ),
-            sierpinski( $subtriangle_b, $recursions_remaining ),
-            sierpinski( $subtriangle_c, $recursions_remaining ),
-        ];
+        # construct 3 sub-triangles from orinal points and newly-calculated midpoints
+        my number::arrayref::arrayref $subtriangle_a = [ $point_a,   $point_a_b, $point_a_c ];
+        my number::arrayref::arrayref $subtriangle_b = [ $point_a_b, $point_b,   $point_b_c ];
+        my number::arrayref::arrayref $subtriangle_c = [ $point_a_c, $point_b_c, $point_c   ];
+
+        # $triangle_groups is zero-indexed like all other Perl arrays,
+        # so we need to subtract one from $recursions_remaining before using as an index,
+        # in order to avoid an undefined element at element 0;
+        # also, we need to decrement $recursions_remaining before making recursive calls;
+        # for both of these reasons, we can decrement now
+        $recursions_remaining--;
+
+        # store all triangles grouped by recursion level
+        push @{$triangle_groups->[$recursions_remaining]}, $subtriangle_a;
+        push @{$triangle_groups->[$recursions_remaining]}, $subtriangle_b;
+        push @{$triangle_groups->[$recursions_remaining]}, $subtriangle_c;
+
+        # recurse once for each sub-triangle
+        sierpinski( $subtriangle_a, $recursions_remaining, $triangle_groups);
+        sierpinski( $subtriangle_b, $recursions_remaining, $triangle_groups);
+        sierpinski( $subtriangle_c, $recursions_remaining, $triangle_groups);
     }
-
-    return $return_value;
 }
+
 
 sub draw_triangle {
     { my void $RETURN_TYPE };
